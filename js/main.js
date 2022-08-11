c65909b2a82437ae595f6adc534b2e3c581dd03c
melad99 <melad1999@gmail.com>
2022-08-11 14:16:25 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function faq1() {
+	var ans1 = document.getElementById('ans1');
+
+	if (ans1.style.display === 'none') {
+		ans1.style.display = 'block';
+		document.getElementById('q1').style.fontWeight = 'bold';
+
+	} else {
+		ans1.style.display = 'none';
+		document.getElementById('q1').style.fontWeight = 'normal';
+	}
+}
+function faq2() {
+	var ans1 = document.getElementById('ans2');
+
+	if (ans2.style.display === 'none') {
+		ans2.style.display = 'block';
+		document.getElementById('q2').style.fontWeight = 'bold';
+	} else {
+		ans2.style.display = 'none';
+		document.getElementById('q2').style.fontWeight = 'normal';
+	}
+}
+function faq3() {
+	var ans1 = document.getElementById('ans3');
+
+	if (ans3.style.display === 'none') {
+		ans3.style.display = 'block';
+		document.getElementById('q3').style.fontWeight = 'bold';
+
+	} else {
+		ans3.style.display = 'none';
+		document.getElementById('q3').style.fontWeight = 'normal';
+	}
+}
+function faq4() {
+	var ans1 = document.getElementById('ans4');
+
+	if (ans4.style.display === 'none') {
+		ans4.style.display = 'block';
+		document.getElementById('q4').style.fontWeight = 'bold';
+
+	} else {
+		ans4.style.display = 'none';
+		document.getElementById('q4').style.fontWeight = 'normal';
+	}
+}
+function faq5() {
+	var ans5 = document.getElementById('ans5');
+
+	if (ans5.style.display === 'none') {
+		ans5.style.display = 'block';
+		document.getElementById('q5').style.fontWeight = 'bold';
+
+	} else {
+		ans5.style.display = 'none';
+		document.getElementById('q5').style.fontWeight = 'normal';
+	}
+}
